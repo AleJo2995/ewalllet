50e57546ba4cd22eec4c9f6fffdd025eec5e5806
DESKTOP-OR2GI5O\User <alejandroleiv@hotmail.com>
2021-04-05 20:29:20 -0500

    Adding table name configurable for roles per user to be used on bulk insert

--- data-service/config.js
+++ data-service/config.js
@@ -24,6 +24,7 @@ module.exports = {
         options: {
             encrypt: sqlEncrypt,
             enableArithAbort:true
-        }
+        },
+        rolesUserTableName: 'rol_x_usuario'
     }
 }
