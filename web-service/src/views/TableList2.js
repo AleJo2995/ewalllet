1ff54c57d8627d3505aba852cacbc4dde6467e17
DESKTOP-OR2GI5O\User <alejandroleiv@hotmail.com>
2021-04-24 00:00:51 -0500

    last changes

--- web-service/src/views/TableList2.js
+++ web-service/src/views/TableList2.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import config from "../config.json";
+import { store } from 'react-notifications-component';
 
 // react-bootstrap components
 import {
@@ -12,78 +14,114 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
+import MUIDataTable from "mui-datatables";
+import axios from "axios";
 
 function TableList2() {
+
+  useEffect(() => {
+    getRoutes();
+    setRoutesColumns([
+      {
+        name: "nombre",
+        label:"Nombre",
+        options: {
+         filter: true,
+         sort: false
+        }
+      },
+      {
+        name: "descripcion",
+        label:"Descripcion",
+        options: {
+         filter: true,
+         sort: false
+        }
+      },
+      {
+          name: "costo",
+          label:"Costo",
+          options: {
+           filter: true,
+           sort: false
+          }
+      }]);
+  }, []);
+
+  const { SERVER_URL } = config;
+  const [user, setUserData] = useState('');
+  const [walletId, setId] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
+  const [name, setName] = useState('');
+  const [cedula, setCedula] = useState('');
+  const [saldo, setSaldo] = useState('');
+  const [newBalance, setNewBalance] = useState('');
+  const [showSuccess, setSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [errors, setErrors] = useState({}); 
+  const [rutas, setRutas] = useState('');
+  const [routesColumns, setRoutesColumns] = useState([]);
+  const [routeToAdd, setRouteToAdd] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const options = {
+    filterType: 'checkbox',
+    rowsPerPage:5
+  };
+
+  const getRoutes = () => {
+    axios.get(SERVER_URL + '/routes/getAll')
+    .then((response) => {
+      // handling success
+      setRutas(response.data);
+      const userData = JSON.parse(localStorage.getItem('user'));
+    setUserData(userData);
+     
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      store.addNotification({
+        title: "Error al traer las rutas",
+        message: error.message,
+        type: "danger",
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 5000,
+          onScreen: true
+        }
+      });
+    })
+  }
+
+  const data = [
+    ["Joe James", "Test Corp", "Yonkers", "NY"],
+    ["John Walsh", "Test Corp", "Hartford", "CT"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL"],
+    ["James Houston", "Test Corp", "Dallas", "TX"],
+   ];
+
+   const columns = ["Name", "Company", "City", "State"];
+
   return (
     <>
       <Container fluid>
         <Row>
-          <Col md="12">
-            <Card className="strpied-tabled-with-hover">
+        <Col md="5">
+            <Card>
               <Card.Header>
-                <Card.Title as="h4">Gastos</Card.Title>
-      
+                <Card.Title as="h4">Rutas utilizadas recientemente por {user.nombre}</Card.Title>
               </Card.Header>
-              <Card.Body className="table-full-width table-responsive px-0">
-                <Table className="table-hover table-striped">
-                  <thead>
-                    <tr>
-                      <th className="border-0">ID</th>
-                      <th className="border-0">Monto</th>
-                      <th className="border-0">Mes</th>
-    
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>1000</td>
-                      <td>01</td>
-                
-               
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>2000</td>
-                      <td>02</td>
-                
-           
-                    </tr>
-
-  
-                  </tbody>
-                </Table>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md="12">
-            <Card className="card-plain table-plain-bg">
-              <Card.Header>
-                <Card.Title as="h4">Rutas Utilizadas</Card.Title>
-        
-              </Card.Header>
-              <Card.Body className="table-full-width table-responsive px-0">
-                <Table className="table-hover">
-                  <thead>
-                    <tr>
-                      <th className="border-0">ID</th>
-                      <th className="border-0">Ruta</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>SJ-SA</td>
-
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Cartago-Alajuela</td>
-            
-                    </tr>
-
-                  </tbody>
-                </Table>
+              <Card.Body>
+              <MUIDataTable
+                title={"Rutas Utilizadas"}
+                data={rutas ? rutas : data}
+                columns={routesColumns}
+                options={options}
+              />
               </Card.Body>
             </Card>
           </Col>
